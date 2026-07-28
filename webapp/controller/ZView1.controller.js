c6939aeeb1fa78a1b556662967832143e63e7b58
manik.saluja@in.abb.com <manik.saluja@in.abb.com>
2026-07-28 17:23:42 +0000

    changes for the zview1

--- webapp/controller/ZView1.controller.js
+++ webapp/controller/ZView1.controller.js
@@ -7,6 +7,10 @@ function (Controller) {
     return Controller.extend("com.cfin.zcfinmjedashapr.controller.ZView1", {
         onInit: function () {
 
+        },
+        onPressDocNo:function(oEvent){
+            var oBj = oEvent.getSource().getBindingContext().getObject();
+            console.log(oBj);
         }
     });
 });
